24d196711c0da2abb568eb5d1096eb8d0be1ff26
Misha <msharma343@gatech.edu>
2025-10-02 14:26:14 -0400

    updated screen sizing

--- src/templates/article.js
+++ src/templates/article.js
@@ -16,33 +16,43 @@ function Article({data}) {
         <Layout>
             <title>{[post.title]}</title>
             <div className="articleContainer">
-            {post.relationships.field_audio && 
-                <div className="articleImageContainer">
-                    <img className="articleImage" src={post.relationships.field_image.localFile.url}></img>
-                    <div className="articleTitleInfo">
-                        <h1 className="articleTitle">{post.title}</h1>
-                        <h3 className="authorInfo"> By {post.field_author}</h3>
-                        <audio className="articleAudio" src = {post.relationships.field_audio != null ? "https://empathybytes.library.gatech.edu" + post.relationships.field_audio.path.alias : null} controls>
-                        </audio>
+                {post.relationships.field_audio &&
+                    <div className="articleImageContainer">
+                        <div className="imageFrame">
+                            <img className="articleImage" src={post.relationships.field_image.localFile.url} alt={post.title} />
+                            <div className="articleTitleInfo">
+                                <h1 className="articleTitle">{post.title}</h1>
+                                <h3 className="authorInfo"> By {post.field_author}</h3>
+                            </div>
+                            <audio className="articleAudio"
+                            src={
+                                post.relationships.field_audio != null
+                                ? "https://empathybytes.library.gatech.edu" +
+                                post.relationships.field_audio.path.alias : null
+                            } controls />
+                        </div>
                     </div>
+                }
+            {post.field_video_url ? (
+                <div className="videoWrapper">
+                    <iframe className="articleVideo"
+                    src={ post.field_video_url.includes("youtube.com/watch")
+                        ? post.field_video_url.replace("watch?v=", "embed/")
+                        : post.field_video_url
+                    }
+                    title={post.title}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                    ></iframe>
                 </div>
-            }
-
-            { post.field_video_url ?
-                <iframe className="articleVideo" width="887" height="499" 
-                    src={post.field_video_url}>
-                </iframe> : null
-            }
-
+            ) : null}
 
             <div className="articleText" dangerouslySetInnerHTML={{ __html: post.body.processed }}></div>
-            
-            
-            <div class="clicker" tabindex="1">View Transcript</div>
-            <div class="hiddendiv">
-                <div className="articleText" dangerouslySetInnerHTML={{ __html: post.field_transcript.processed }}></div>
-            </div>
-            </div>
+            <div className="clicker" tabIndex={1}>View Transcript</div>
+            <div class="hiddendiv"> <div className="articleText" dangerouslySetInnerHTML={{ __html: post.field_transcript.processed }}></div>
+        </div>
+        </div>
         </Layout>
     );
 }

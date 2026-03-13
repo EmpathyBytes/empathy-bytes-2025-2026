bda8c6fe6f2ba04ede97f710110a77f1ee464bf1
thestephaniewu1 <swu460@gatech.edu>
2026-03-13 13:47:33 -0400

    Added Olympic Gallery and Timeline links to nav

---
+++ src/pages/olympic-timeline.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function OlympicTimelinePage() {
+  return (
+    <Layout>
+      <h1>Olympic Timeline</h1>
+      <p>Coming soon.</p>
+    </Layout>
+  );
+}

b202fe879a2e2d762201910426446cbb9b2a1315
thestephaniewu1 <swu460@gatech.edu>
2026-04-15 14:12:29 -0400

    Created new file and added media into images.

---
+++ src/pages/audio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../components/button";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import { graphql } from "gatsby" 
+
+import Grid from '@mui/material/Grid';
+
+
+
+function AudioPage() {
+    return (
+
+    );
+}
+
+export default App;
